95da1f4aad8931ab30a88cd5157ca21976e555e8
Md Hasibul Hassan Shanto <157917726+shanto812@users.noreply.github.com>
2026-02-26 22:55:17 +0600

    Reverse-Array-In-place

---
+++ task5.java
@@ -0,0 +1,15 @@
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        for (int i = 0; i < n / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n - 1 - i];
+            arr[n - 1 - i] = temp;
+        }
+        for (int num : arr) System.out.print(num + " ");
+    }
+}
+
