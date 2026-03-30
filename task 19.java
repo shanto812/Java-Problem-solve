9d152a0444ca5e9f6aaa832c0718d03e54a6b811
Md Hasibul Hassan Shanto <157917726+shanto812@users.noreply.github.com>
2026-03-30 11:04:35 +0600

    Task-19
    Human

---
+++ task 19.java
@@ -0,0 +1,31 @@
+public class Human{
+public int age;
+public double height;
+
+Human h1 = new Human();
+Human h2 = new Human();
+h1.age = 21;
+h1.height = 5.5;
+System.out.println(h1.age);
+System.out.println(h1.height);
+h2.height = h1.height - 3;
+System.out.println(h2.height);
+h2.age = h1.age++;
+System.out.println(h1.age);
+h2 = h1;
+System.out.println(h2.age);
+System.out.println(h2.height);
+h2.age++;
+h2.height++;
+System.out.println(h1.age);
+System.out.println(h1.height);
+h1.age = ++h2.age;
+System.out.println(h2.age);
+System.out.println(h2.height);
+
+}
+
+public class Human {
+    public int age;
+    public double height;
+}
